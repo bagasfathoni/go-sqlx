cf2830d931e7d50c53455c38c2fd2d8d130cd744
Bagas Adi Fathoni <bagasadev@outlook.com>
2022-07-09 12:22:57 +0700

    create advisor struct

---
+++ model/advisor.go
@@ -0,0 +1,17 @@
+package model
+
+import "encoding/json"
+
+type Advisor struct {
+	ID    int32
+	Name  string
+	Email string
+}
+
+func (a *Advisor) ToJSON() string {
+	advisor, err := json.MarshalIndent(a, "", " ")
+	if err != nil {
+		return ""
+	}
+	return string(advisor)
+}
