e10e96a6c8f5c9167c2aa75c22f3d48db8711186
Bagas Adi Fathoni <bagasadev@outlook.com>
2022-07-09 15:03:08 +0700

    add sql queries (crud) for department

--- config/db_query.go
+++ config/db_query.go
@@ -7,4 +7,10 @@ const (
 	SELECT_ADVISOR      = `select * from advisors where $1=$2`
 	INSERT_ADVISOR      = `insert into advisors(name,email) values ($1,$2)`
 	UPDATE_ADVISOR      = `update advisors set $3=$4 where $1=$2`
+
+	// Department queries
+	SELECT_ALL_DEPARTMENTS = `select * from departments`
+	SELECT_DEPARTMENTS     = `select * from departments where $1=$2`
+	INSERT_DEPARTMENTS     = `insert into departments(name,school) values ($1,$2)`
+	UPDATE_DEPARTMENTS     = `update advisors set $3=$4 where $1=$2`
 )
